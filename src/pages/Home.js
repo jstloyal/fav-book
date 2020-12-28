dd01f27779db22f932aaca1952d7a1594ea0a223
jstloyal <jstloyalty@gmail.com>
2020-12-28 10:30:49 +0100

    add footer and style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import { useSelector } from 'react-redux';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
-import NewBooks from '../features/catalog/NewBooks';
-import FavoritedBooks from '../features/catalog/FavoritedBooks';
+// import NewBooks from '../features/catalog/NewBooks';
+// import FavoritedBooks from '../features/catalog/FavoritedBooks';
 import HeroSection from '../components/HeroSection';
 import DashboardSection from '../components/DashboardSection';
 import DesignerSection from '../components/DesignerSection';
 import PeopleSection from '../components/PeopleSection';
+import StartFreeSection from '../components/StartFreeSection';
+import Footer from '../components/Footer';
 
 const Home = () => {
   const loading = useSelector(state => state.catalog.loaders.loadingBooks);
@@ -22,8 +24,8 @@ const Home = () => {
       <DashboardSection />
       <DesignerSection />
       <PeopleSection />
-      <FavoritedBooks />
-      <NewBooks />
+      <StartFreeSection />
+      <Footer />
     </div>
   );
 };
