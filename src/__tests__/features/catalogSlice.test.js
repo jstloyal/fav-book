db9ebbafd48808f4be2b7056ca873a29ea26fd68
jstloyal <jstloyalty@gmail.com>
2020-12-31 17:27:44 +0100

    fix liters error

--- src/__tests__/features/catalogSlice.test.js
+++ src/__tests__/features/catalogSlice.test.js
@@ -5,14 +5,7 @@ import { render, cleanup } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../../app/store';
 import App from '../../App';
-import { login, logout } from '../../features/user/userSlice';
-import {
-  getBooks,
-  getBook,
-  addBook,
-  deleteBook,
-  favorite,
-} from '../../features/catalog/catalogSlice';
+import { getBooks, getBook } from '../../features/catalog/catalogSlice';
 
 afterEach(() => {
   cleanup();
